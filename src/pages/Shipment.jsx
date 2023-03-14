5e7734bd20652e268fa9fb7d7accecf1f5600d81
AYla.O <bgp962@myy.haaga-helia.fi>
2023-03-14 15:41:44 +0200

    skeletons for admin, delivery and account updated

--- src/pages/Shipment.jsx
+++ src/pages/Shipment.jsx
@@ -2,24 +2,26 @@ import { Grid, Button } from "@mui/material";
 import { useState, useEffect } from "react";
 import MuiFormWrapper from "../components/common/forms/MuiFormWrapper";
 
-const Deliveries = () => {
-  const [deliveries, setDeliveries] = useState([]);
+const Shipment = () => {
+
+  const [shipments, setShipments] = useState([]);
 
   useEffect(() => {
-    async function fetchDeliveries() {
+    async function fetchShipments() {
       const response = await fetch("");
       const data = await response.json();
-      setDeliveries(data);
+      setShipments(data);
     }
-    fetchDeliveries();
+    fetchShipments();
   }, []);
 
-  const inDelivery = deliveries.filter(
-    (delivery) => delivery.status === "ordered"
+  const inDelivery = shipments.filter(
+    (shipment) => shipment.status === "ordered"
   );
+
   //assuming that there is a status
-  const completed = deliveries.filter(
-    (delivery) => delivery.status === "delivered"
+  const completed = shipments.filter(
+    (shipment) => shipment.status === "shipped"
   );
 
   return (
@@ -29,6 +31,11 @@ const Deliveries = () => {
       <Grid container spacing={2}>
         <Grid xs={6} md={8} item>
         <MuiFormWrapper headerText="In Delivery"/>
+        <ul>
+            {inDelivery.map(shipment => (
+          <li key={shipment.id}>{shipment.name}</li>
+        ))}
+      </ul>
         <Button
             type="submit"
             fullWidth
@@ -45,6 +52,11 @@ const Deliveries = () => {
       <Grid container spacing={2}>
         <Grid xs={6} md={8} item>
         <MuiFormWrapper headerText="Completed"/>
+        <ul>
+            {completed.map(shipment => (
+          <li key={shipment.id}>{shipment.name}</li>
+        ))}
+      </ul>
         </Grid>
         <Grid xs={6} md={4} item></Grid>
       </Grid>
@@ -52,4 +64,4 @@ const Deliveries = () => {
   );
 };
 
-export default Deliveries;
+export default Shipment;
