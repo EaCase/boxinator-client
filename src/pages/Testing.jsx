604ce6668759828be30f69ae0dbfb2554a2a786a
Mika Hoffren <mika.hoffren@helsinki.fi>
2023-03-14 15:13:06 +0200

    Add dummy data for testing

--- src/pages/Testing.jsx
+++ src/pages/Testing.jsx
@@ -1,7 +1,6 @@
 import {
   Container,
   Paper,
-  Tab,
   Table,
   TableBody,
   TableCell,
@@ -23,8 +22,11 @@ const Testing = () => {
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
               <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell align="right">Size</TableCell>
+                <TableCell>Id</TableCell>
+                <TableCell align="right">Name</TableCell>
+                <TableCell align="right">Country</TableCell>
+                <TableCell align="right">Status</TableCell>
+                <TableCell align="right">Date</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -33,10 +35,24 @@ const Testing = () => {
                   key={row.name}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                 >
-                  <TableCell component="th" scope="row">
+                  <TableCell component="td">
+                    {row.id}
+                  </TableCell>
+                  <TableCell component="td" align="right">
                     {row.name}
                   </TableCell>
-                  <TableCell align="right">{row.size}</TableCell>
+                  <TableCell component="td" align="right">
+                    {row.country}
+                  </TableCell>
+                  <TableCell component="td" align="right">
+                    {row.status}
+                  </TableCell>
+                  <TableCell component="td" align="right">
+                    {row.date}
+                  </TableCell>
+                  <TableCell component="td" align="right">
+                    {row.size}
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
