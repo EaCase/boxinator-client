e50de6dc3c574641e25efcb5e8318965e2a97731
AYla.O <bgp962@myy.haaga-helia.fi>
2023-03-20 16:08:23 +0200

    login kinda works maybe

--- src/services/api.js
+++ src/services/api.js
@@ -3,11 +3,11 @@ import keycloak from '../keycloak'
 
 // Create our baseQuery instance
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:3001/',
+  baseUrl: 'http://localhost:8080/',
   prepareHeaders: (headers, { getState }) => {
     // By default, if we have a token in the store, let's use that for authenticated requests
-    //const token = (getState()).auth.token
-    const { token } = keycloak
+    const token = (getState()).auth.token
+    //const { token } = keycloak
 
     if (token) {
       headers.set('authentication', `Bearer ${token}`)
