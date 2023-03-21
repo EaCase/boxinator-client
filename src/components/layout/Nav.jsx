89bf6ac8dc55e276310beb8506e1e5bf374f0273
hoffrenm <46321023+hoffrenm@users.noreply.github.com>
2023-03-21 14:36:27 +0200

    Remove unused components

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -11,7 +11,6 @@ import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import { Link } from "react-router-dom";
-import keycloak from "../../keycloak";
 
 const pages = ["Login", "Register", "Shipments", "Account", "Admin"];
 
@@ -31,25 +30,23 @@ function ResponsiveAppBar() {
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Link to={"/"}>
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              LOGO
-            </Typography>
-          </Link>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
@@ -116,13 +113,6 @@ function ResponsiveAppBar() {
                 </Button>
               </Link>
             ))}
-
-            <Button
-              sx={{ my: 2, color: "white", display: "block" }}
-              onClick={() => keycloak.login()}
-            >
-              LOGGAIN
-            </Button>
           </Box>
         </Toolbar>
       </Container>
