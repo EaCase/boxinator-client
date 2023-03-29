2b94e2a7da56bc10a80118bedc69822d055c9aaf
hoffrenm <46321023+hoffrenm@users.noreply.github.com>
2023-03-29 12:29:55 +0300

    Display message if no shipments are present

--- src/pages/Shipment.jsx
+++ src/pages/Shipment.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Grid, Box, Typography } from "@mui/material";
+import { Grid, Box, Typography, Paper, Divider } from "@mui/material";
 import {
   useGetCancelledShipmentsQuery,
   useGetCompletedShipmentsQuery,
@@ -9,8 +9,10 @@ import OrderModal from "../components/Shipment/OrderModal";
 import OrderCarousel from "../components/Shipment/OrderCarousel";
 import CompletedOrder from "../components/Shipment/CompletedOrder";
 import OrderForm from "../components/Shipment/OrderForm";
+import { useTheme } from "@emotion/react";
 
 const Shipment = () => {
+  const theme = useTheme();
   const { data: pendingShipments, isSuccess: pendingShipmentsFetched } =
     useGetShipmentsQuery({});
   const { data: completedShipments, isSuccess: completedShipmentsFetched } =
@@ -22,13 +24,15 @@ const Shipment = () => {
   const closeModal = () => setShowModal(false);
   const openModal = () => setShowModal(true);
 
+  console.log(completedShipments);
+
   return (
     <Box mt={5}>
       <OrderModal showModal={showModal} closeModal={closeModal}>
         <OrderForm title="Create a new order" />
       </OrderModal>
-      <Box>
-        <Typography variant="h3" sx={{ mb: 3, marginTop: "20px" }}>
+      <Box my={5}>
+        <Typography variant="h3" sx={{ my: 4 }}>
           Pending orders
         </Typography>
         {pendingShipmentsFetched && (
@@ -36,26 +40,58 @@ const Shipment = () => {
         )}
       </Box>
 
-      <Box>
-        <Typography variant="h3" sx={{ mb: 3, marginTop: "20px" }}>
+      <Divider flexItem />
+
+      <Box my={5}>
+        <Typography variant="h3" sx={{ my: 4 }}>
           Completed orders
         </Typography>
 
         <Grid container>
-          {completedShipmentsFetched &&
+          {completedShipments && completedShipments.length !== 0 ? (
             completedShipments.map((shipment) => (
               <CompletedOrder key={shipment.id} {...shipment} />
-            ))}
+            ))
+          ) : (
+            <Typography
+              variant="h6"
+              sx={{
+                mb: 2,
+                ml: 3,
+                color: theme.palette.secondary.main,
+                fontStyle: "italic",
+              }}
+            >
+              Your completed shipments will show here
+            </Typography>
+          )}
         </Grid>
-
-        <Typography variant="h3" sx={{ mb: 3, marginTop: "20px" }}>
+      </Box>
+      <Divider flexItem />
+      <Box>
+        <Typography variant="h3" sx={{ my: 4 }}>
           Cancelled orders
         </Typography>
         <Grid container>
-          {cancelledShipmentsFetched &&
-            cancelledShipments.map((shipment) => (
-              <CompletedOrder key={shipment.id} {...shipment} />
-            ))}
+          <Grid container>
+            {cancelledShipments && cancelledShipments.length !== 0 ? (
+              cancelledShipments.map((shipment) => (
+                <CompletedOrder key={shipment.id} {...shipment} />
+              ))
+            ) : (
+              <Typography
+                variant="h6"
+                sx={{
+                  mb: 3,
+                  ml: 3,
+                  color: theme.palette.secondary.main,
+                  fontStyle: "italic",
+                }}
+              >
+                Your cancelled shipments will show here
+              </Typography>
+            )}
+          </Grid>
         </Grid>
       </Box>
     </Box>
